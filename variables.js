11c32203dc4c49706f279bec4de8164094315709
Eric Weissman <eric@example.com>
2022-06-07 16:09:18 -0600

    Add consale log

---
+++ variables.js
@@ -0,0 +1,16 @@
+var firstName = "Jedeo"
+var lastName = "Manirikumwenatwe"
+var age = 27
+var birtYear = 1994
+var inTuring = true
+var gotJob = false
+
+firstName = "jmani"
+lastName = "I dont really know"
+
+console.log(firstName)
+console.log(lastName)
+console.log(age)
+console.log(inTuring)
+console.log(gotJob)
+console.log(birtYear)
